fabdeaa89af9e71f757079972631bce4857a5353
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2023-07-21 11:14:27 +0200

    Create data.js

---
+++ src/Main/data.js
@@ -0,0 +1,15 @@
+const data = [
+  {title: 'Small array of objects (JSON)', url: 'https://gist.githubusercontent.com/fatihacet/4247503/raw/75df225125cd2ef1819e64b02b00c97bc4096c79/sample.json'},
+  {title: 'Small feed example (JSON)', url: 'https://www.manton.org/feed.json'},
+  {title: 'Bigger array (JSON) 25mb+', url: 'https://raw.githubusercontent.com/json-iterator/test-data/master/large-file.json'},
+  {title: 'Sports feed data', url: 'https://adlayer-dev.icmarketing.se/public/feeds/heroic.json'},
+  {title: '300mb+ data (JSON)', url: 'https://chronicdata.cdc.gov/views/6vp6-wxuq/rows.json?accessType=DOWNLOAD'},
+  {title: 'Google Products (XML)', url: 'https://adlayer-dev.icmarketing.se/public/feeds/CC_ProductDataFeed-GoogleProducts.xml'},
+  {title: 'HeroicGaming Feed (XML)', url: 'http://adlayer-dev.icmarketing.se/public/feeds/heroicgaming_entire_feed.xml'},
+  {title: 'Small shop (XML)', url: 'http://adlayer-dev.icmarketing.se/public/feeds/smallshop.xml'},
+  {title: 'Single tag (XML)', url: 'https://www.w3schools.com/xml/note.xml'},
+  {title: 'Data (CSV)', url: 'https://adlayer-dev.icmarketing.se/public/feeds/asok6969.csv'}
+];
+
+export default data;
+
